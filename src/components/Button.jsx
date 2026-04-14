9587d48b6a76574f2353d8b3850dc2e22d0b224d
Jazmín Hernández <li.jhernandezz@gmail.com>
2026-04-13 19:04:32 -0600

    feat: add calculator buttons structure and basic styling

---
+++ src/components/Button.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import '../styles/Button.css'
+
+function Button(props) {
+
+  const isOperator = value => {
+    return isNaN(value) && (value != '.') && (value != '='); 
+  };
+
+  return (
+    <div 
+      className={`button-container ${isOperator(props.children) ? 'operator' : ''}`.trimEnd()} >
+      {props.children}
+    </div>
+      )
+}
+
+export default Button
